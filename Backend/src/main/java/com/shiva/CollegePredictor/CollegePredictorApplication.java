8ed3fb5eed1a06a2726830914bbe836cdd042deb
Shiva20075 <palleshiva2007@gmail.com>
2026-03-05 19:14:28 +0530

    Initial Commit

---
+++ Backend/src/main/java/com/shiva/CollegePredictor/CollegePredictorApplication.java
@@ -0,0 +1,13 @@
+package com.shiva.CollegePredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegePredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegePredictorApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/shiva/CollegePredictor/CollegePredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiva.CollegePredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegePredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
